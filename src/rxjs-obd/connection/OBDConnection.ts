db0bfacb2ecd69f1841f44072eed3085a344cfe5
Bernardo Baumblatt <bernardob@br-petrobras.com.br>
2018-02-22 15:50:31 -0300

    The OBD abstract WIFI connection.

---
+++ src/rxjs-obd/connection/OBDConnection.ts
@@ -0,0 +1,30 @@
+import {Observable} from 'rxjs';
+
+export abstract class OBDConnection {
+	/**
+	 * Open connection to OBD Reader.
+	 * @returns {Observable} the observable of opening connection.
+	 * The events emitted are ignored, wait for complete (successful) or error (failure).
+	 */
+	abstract open(config: any): Observable<any>;
+
+	/**
+	 * Send a command to OBD Reader.
+	 * @returns {Observable} the observable of send operation.
+	 * The events emitted are ignored, wait for complete (successful) or error (failure).
+	 */
+	abstract send(command: string): Observable<any>;
+
+	/**
+	 * Listener for data received from OBD Reader.
+	 * @returns {Observable<string>} Observable of data received from OBD Reader.
+	 */
+	abstract onData(): Observable<string>;
+
+	/**
+	 * Close and dispose resources of OBD Reader connection.
+	 * @returns {Observable} the observable of close operation.
+	 * The events emitted are ignored, wait for complete (successful) or error (failure).
+	 */
+	abstract close(): Observable<any>;
+}
---
+++ src/rxjs-obd/connection/OBDWifiConfig.ts
@@ -0,0 +1,41 @@
+import {Observable} from 'rxjs';
+import {empty} from 'rxjs/observable/empty';
+import {of} from 'rxjs/observable/of';
+import {OBDConnection} from './OBDConnection';
+
+export interface OBDWifiConfig {
+	host: string;
+	port: number;
+	pullingInterval: number;
+	connection: OBDConnection;
+}
+
+export class DummyOBDConnection extends OBDConnection {
+
+	open(config: any): Observable<any> {
+		console.log('DummyOBDConnection.open');
+		return empty();
+	}
+
+	send(): Observable<any> {
+		console.log('DummyOBDConnection.send');
+		return empty();
+	}
+
+	onData(): Observable<string> {
+		return of('dummy');
+	}
+
+	close(): Observable<any> {
+		console.log('DummyOBDConnection.close');
+		return empty();
+	}
+}
+
+export const defaultOBDWifiConfig: OBDWifiConfig = {
+	host: '192.168.0.10',
+	port: 35000,
+	pullingInterval: 1000,
+	connection: new DummyOBDConnection()
+};
+
