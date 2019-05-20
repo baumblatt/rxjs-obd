05966dfc783ed63d89c3a54fa45c94038774aa35
baumblatt <baumblatt@gmail.com>
2019-05-20 03:04:27 -0300

    Ethanol Level

--- src/model/OBDFields.ts
+++ src/model/OBDFields.ts
@@ -2,6 +2,7 @@ import {OBDField} from './OBDField';
 
 const integer = (unit: string, value: string | number) => `${Math.round(<number>value)} ${unit}`;
 
+export const ETHANOLLEVEL = new OBDField('Ethanol Level', '%', 0, integer);
+export const FUELTANKLEVEL = new OBDField('Fuel Tank Level', '%', 0, integer);
 export const RPM = new OBDField('Engine RPM', 'rpm', 0);
 export const VEHICLESPEED = new OBDField('Vehicle Speed', 'km/h', 0);
-export const FUELTANKLEVEL = new OBDField('Fuel Tank Level', '%', 0, integer);
